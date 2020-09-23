b4ad9225f09c2fcb15830044d9df22e07c2960c4
Cathy Chan <chan.cathy@hotmail.ca>
2020-09-23 01:28:35 -0700

    route53 ddns support added

--- scripts/amazon/dynamic_dns.sh
+++ scripts/amazon/dynamic_dns.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 source /foundryssl/variables.sh
 
+# retrieve public ip
+public_ip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+
+# get IP for subdomain record
+aws_ip=`aws route53 list-resource-record-sets --hosted-zone-id ${zone_id} | jq ".ResourceRecordSets[] | select(.Name==\"${fqdn}\") | select(.Type==\"A\") | .ResourceRecords[] | .Value" | cut -d '"' -f2`
+
+# if dont match then create temp dns_block.json replacing domain_here and ip_here with values
+if [ "${public_ip}" != "${aws_ip}" ]
+then
+    # change subdomain record
+    aws route53 change-resource-record-sets --hosted-zone-id ${zone_id} --change-batch "{ \"Comment\": \"Dynamic DNS change\", \"Changes\": [ { \"Action\": \"UPSERT\", \"ResourceRecordSet\": { \"Name\": \"${subdomain}.${fqdn}\", \"Type\": \"A\", \"TTL\": 120, \"ResourceRecords\": [ { \"Value\": \"${public_ip}\" } ] } } ] }"
+    if [ "${webserver_bool}" == "True" ]
+    then
+        aws route53 change-resource-record-sets --hosted-zone-id ${zone_id} --change-batch "{ \"Comment\": \"Dynamic DNS change\", \"Changes\": [ { \"Action\": \"UPSERT\", \"ResourceRecordSet\": { \"Name\": \"${fqdn}\", \"Type\": \"A\", \"TTL\": 120, \"ResourceRecords\": [ { \"Value\": \"${public_ip}\" } ] } } ] }"
+    fi
+fi
--- scripts/google/record_set.sh
+++ scripts/google/record_set.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-source /foundryssl/variables.sh
-sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install -y ddclient
 
+# grab variables
+source /foundryssl/variables.sh
 if [[ ${webserver_bool} == "True" ]]
 then
     client_conf="ddclient_webserver.conf"
@@ -10,6 +9,11 @@ else
     client_conf="ddclient.conf"
 fi
 
+# install ddclient
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install -y ddclient
+
+# set ddclient config
 sudo sed -i "s/api_key/${api_key}/g" /aws-foundry-ssl/files/ddns/google/${client_conf}
 sudo sed -i "s/api_secret/${api_secret}/g" /aws-foundry-ssl/files/ddns/google/${client_conf}
 sudo sed -i "s/subdomain/${subdomain}/g" /aws-foundry-ssl/files/ddns/google/${client_conf}
@@ -23,6 +27,7 @@ fi
 
 sudo cat /aws-foundry-ssl/files/ddns/google/${client_conf} >> /etc/ddclient.conf
 
+# restart ddclient
 sudo systemctl start ddclient
 sudo systemctl enable ddclient
 
