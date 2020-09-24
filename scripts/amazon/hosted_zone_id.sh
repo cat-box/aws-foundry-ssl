a3f6f80e6ade72b6b2a16ac7249da48caf3f2b4b
Cathy Chan <chan.cathy@hotmail.ca>
2020-09-23 23:23:53 -0700

    amazon ddns fix

--- scripts/amazon/hosted_zone_id.sh
+++ scripts/amazon/hosted_zone_id.sh
@@ -6,7 +6,7 @@ zone_id=`aws route53 list-hosted-zones | jq ".HostedZones[] | select(.Name==\"${
 
 echo "zone_id=${zone_id}" >> /foundryssl/variables.sh
 
-sudo cp /aws-foundry-ssl/scripts/amazon/dynamic_dns.sh /foundryssl/amazon/dynamic_dns.sh
-crontab -l | { cat; echo "@reboot    /foundryssl/amazon/dynamic_dns.sh > /dev/null"; } | crontab -
-crontab -l | { cat; echo "*/10 * * * *    /foundryssl/amazon/dynamic_dns.sh > /dev/null"; } | crontab -
+sudo cp /aws-foundry-ssl/scripts/amazon/dynamic_dns.sh /foundryssl/dynamic_dns.sh
+crontab -l | { cat; echo "@reboot    /foundryssl/dynamic_dns.sh > /dev/null"; } | crontab -
+crontab -l | { cat; echo "*/10 * * * *    /foundryssl/dynamic_dns.sh > /dev/null"; } | crontab -
 
