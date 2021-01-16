2bf677754d539c2284b1950d15f87f8d3be595c1
Cathy Chan <chan.cathy@hotmail.ca>
2021-01-15 21:24:49 -0800

    escape special characters

--- patches/certbot_cronjob.sh
+++ patches/certbot_cronjob.sh
@@ -21,4 +21,4 @@ echo -e "PATH=/usr/bin:/bin:/usr/sbin\n\n$(cat ${CRON_PATH})" > ${CRON_PATH}
 sed -i "s|@reboot    /usr/bin/certbot renew --quiet|@reboot    ${SCRIPT_PATH}|" ${CRON_PATH}
 
 # replace scheduled renew
-sed -i "s|0 12 * * *     /usr/bin/certbot renew --quiet|0 12 * * *     certbot renew --nginx --no-self-upgrade --no-random-sleep-on-renew --post-hook \"systemctl restart nginx\" > /var/log/foundrycron/certbot_renew_daily.log 2>&1|" ${CRON_PATH}
+sed -i "s|0 12 \* \* \*     /usr/bin/certbot renew --quiet|0 12 \* \* \*     certbot renew --nginx --no-self-upgrade --no-random-sleep-on-renew --post-hook \"systemctl restart nginx\" > /var/log/foundrycron/certbot_renew_daily.log 2>\&1|" ${CRON_PATH}
