4613d26e1dd3bd0c71ec2c2b3b9a5dc6e03ad585
Cathy Chan <chan.cathy@hotmail.ca>
2021-06-30 17:47:07 -0700

    modified topic arn retrieval method

--- scripts/features/sns.sh
+++ scripts/features/sns.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 source /foundryssl/variables.sh
 
-# check if topic_arn is empty
-if [ ! -z "${topic_arn}" ]
+if [ "${sns_type}" != "None" ]
 then
-    ec2id=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-    message="Your Foundry EC2 server ${ec2id} has been active for over 24 hours."
-
-    crontab -l | { cat; echo "0 */24 * * *    aws sns publish --topic-arn ${topic_arn} --message \"${message}\" --region ${region} 2>&1"; } | crontab -
+	topic_results=$(aws sns list-topics --region ${region})
+	topic_arn=$(echo "${topic_results}" | grep -oP "arn:aws:sns:.*:\d*:FoundryNotification")
+	ec2id=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+	
+	message="Your Foundry EC2 server ${ec2id} has been active for over 24 hours."
+	
+	crontab -l | { cat; echo "0 */24 * * *    aws sns publish --topic-arn ${topic_arn} --message \"${message}\" --region ${region} 2>&1"; } | crontab -
 fi
