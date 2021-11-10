ee153a5bf51e1797eddd41945d8a5cb2684bca96
zkkng <56467192+zkkng@users.noreply.github.com>
2021-11-10 12:31:56 -0800

    Updating download handling
    Downloading was failing so I added some retry logic to it. Also put limit on the amount of retrying done.  Might consider exit coding if either retry reaches it's max.

--- scripts/global/foundry.sh
+++ scripts/global/foundry.sh
@@ -10,17 +10,34 @@ source /foundryssl/variables.sh
 
 # download foundry from patreon link or google drive
 cd /home/foundry/foundry-install
-if [[ `echo ${foundry_download_link}  | cut -d '/' -f3` == 'drive.google.com' ]]
-then
+
+
+if [[ `echo ${foundry_download_link}  | cut -d '/' -f3` == 'drive.google.com' ]]; then
     fileid=`echo ${foundry_download_link} | cut -d '/' -f6`
-    sudo wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=${fileid}" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p' > confirm.txt
-    sudo wget --load-cookies cookies.txt -O foundry.zip 'https://docs.google.com/uc?export=download&id='${fileid}'&confirm='$(<confirm.txt) && rm -rf cookies.txt confirm.txt
+    while (( FS_Retry < 4 )) ; do
+        sudo wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=${fileid}" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p' > confirm.txt
+        sudo wget --load-cookies cookies.txt -O foundry.zip 'https://docs.google.com/uc?export=download&id='${fileid}'&confirm='$(<confirm.txt) && rm -rf cookies.txt confirm.txt
+        filesize=$(stat -c%s "./foundry.zip")
+        echo "Size of foundry.zip = $filesize bytes."
+        if (( filesize > 100000000 )); then
+            echo "Filesize seems about right! Proceeding."
+            break
+        else
+            echo "Filesize looking too small. Retrying."
+            ((FS_Retry++))
+        fi
+    done
 else 
     sudo wget -O foundry.zip "${foundry_download_link}"
 fi
 
 unzip -u foundry.zip
-rm foundry.zip
+rm -f foundry.zip
+
+# allow rwx in the Data folder only for ec2-user
+chown -R foundry:foundry /home/foundry/ /foundrydata
+find /foundrydata -type d -exec chmod 765 {} +
+find /foundrydata -type f -exec chmod 664 {} +
 
 # start foundry and add to boot
 sudo cp /aws-foundry-ssl/files/foundry/foundry.service /etc/systemd/system/foundry.service
@@ -29,10 +46,11 @@ sudo systemctl daemon-reload
 sudo systemctl start foundry
 sudo systemctl enable foundry
 
+
 # configure foundry aws json file
 F_DIR='/foundrydata/Config/'
 echo "Start time: $(date +%s)"
-while : ; do
+while (( Edit_Retry < 45 )) ; do
     if [ -d $F_DIR ]; then 
         echo "Directory found time: $(date +%s)"
         sudo cp /aws-foundry-ssl/files/foundry/options.json /foundrydata/Config/options.json
@@ -40,16 +58,11 @@ while : ; do
         sudo sed -i "s|ACCESSKEYIDHERE|${access_key_id}|g" /foundrydata/Config/AWS.json
         sudo sed -i "s|SECRETACCESSKEYHERE|${secret_access_key}|g" /foundrydata/Config/AWS.json
         sudo sed -i "s|REGIONHERE|${region}|g" /foundrydata/Config/AWS.json
+        sudo sed -i 's|"awsConfig":.*|"awsConfig": "/foundrydata/Config/AWS.json",|g' /foundrydata/Config/options.json
         break
     else 
         echo  echo "Directory not found time: $(date +%s)"
+        ((Edit_Retry++))
         sleep 1s
     fi
 done
-# configure foundry options file
-sudo sed -i 's|"awsConfig":.*|"awsConfig": "/foundrydata/Config/AWS.json",|g' /foundrydata/Config/options.json
-
-# allow rwx in the Data folder only for ec2-user
-chown -R foundry:foundry /home/foundry/ /foundrydata
-find /foundrydata -type d -exec chmod 765 {} +
-find /foundrydata -type f -exec chmod 664 {} +
